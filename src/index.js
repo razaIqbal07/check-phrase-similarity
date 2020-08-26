12b030ed86ed7d6609c802e707d61e69422ad9d9
unknown <mailrazaiqbal@gmail.com>
2020-08-26 13:22:17 +0530

    added ignored words

--- src/index.js
+++ src/index.js
@@ -24,6 +24,15 @@ const checkSimilarity = (base, comparisonSentences) => {
     count = 0;
     similarityPercentage = 0;
   });
-  console.log(result);
+  // console.log(result);
   return result;
 };
+
+console.log(
+  checkSimilarity(
+    "Opt for Homeopathy to get your skin color back gently, painlessly, & with no side-effects Link Description- Treat Vitiligo Naturally with Homeopathy. To Book an Appointment, call us at 9167191671",
+    [
+      "This letâ€™s remember that Love yourself, just like your near and dear ones love you. Treat Vitiligo gently and safely, with Homeopathy...",
+    ]
+  )
+);
